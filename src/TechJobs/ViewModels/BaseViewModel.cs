9727eb69506527c32d8a93168aa4283ddef98def
littleperks <chad.perkins@outlook.com>
2018-02-26 20:14:24 -0600

    TechJobsOO
    Complete

---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class BaseViewModel
+    {
+        //Current Column
+        public JobFieldType Column { get; set; }
+
+        //All Columns, for display
+        public List<JobFieldType> Columns { get; set; }
+
+        //View Title
+        public string Title { get; set; } = "";
+
+        public BaseViewModel()
+        {
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
+            {
+                Columns.Add(enumVal);
+            }
+        }
+    }
+}
